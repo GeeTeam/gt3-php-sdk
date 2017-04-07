8b1d7f4fbae42342288e4a0081460d2fe9c4ccb6
breaking <breaking@breakingdeMacBook-Pro.local>
2017-04-07 20:31:28 +0800

    fix

--- web/StartCaptchaServlet.php
+++ web/StartCaptchaServlet.php
@@ -10,9 +10,9 @@ $GtSdk = new GeetestLib(CAPTCHA_ID, PRIVATE_KEY);
 session_start();
 
 $data = array(
-		$user_id => "test", # 网站用户id
-		$client_type => "web", # web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
-		$ip_address => "127.0.0.1" # 请在此处传输用户请求验证时所携带的IP
+		"user_id" => "test", # 网站用户id
+		"client_type" => "web", #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
+		"ip_address" => "127.0.0.1" # 请在此处传输用户请求验证时所携带的IP
 	);
 
 $status = $GtSdk->pre_process($data, 1);
--- web/VerifyLoginServlet.php
+++ web/VerifyLoginServlet.php
@@ -10,9 +10,9 @@ $GtSdk = new GeetestLib(CAPTCHA_ID, PRIVATE_KEY);
 
 
 $data = array(
-        $user_id => $_SESSION['user_id'], # 网站用户id
-        $client_type => "web", # web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
-        $ip_address => "127.0.0.1" # 请在此处传输用户请求验证时所携带的IP
+        "user_id" => $_SESSION['user_id'], # 网站用户id
+        "client_type" => "web", #web:电脑上的浏览器；h5:手机上的浏览器，包括移动应用内完全内置的web_view；native：通过原生SDK植入APP应用的方式
+        "ip_address" => "127.0.0.1" # 请在此处传输用户请求验证时所携带的IP
     );
 
 
